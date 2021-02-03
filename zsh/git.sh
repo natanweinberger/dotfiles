9e20a98f7cf2b96b4a6691464d1c076152ddc744
Natan Weinberger <naweinberger+github@gmail.com>
2021-02-03 09:54:29 -0500

    Add git utils

--- zsh/git.sh
+++ zsh/git.sh
@@ -2,3 +2,18 @@
 cleanup() {
 	git branch --merged | grep -v '^*' | xargs git branch -d
 }
+
+# Get the JIRA ticket ID from the current branch name
+jira() {
+    git branch |
+    grep "*" |   # Find the current branch
+    tr -d '*' | tr -d ' ' |   # Trim the non-branch name characters
+    grep -Eo "\w+/DE-\d+" |   # Isolate the ticket ID
+    sed -e 's/\//\(/g' -e 's/$/)/'  # Convert feat/DE-### to feat(DE-###)
+}
+
+# Run `git commit` with the ticket ID prefix
+commit() {
+    [[ -n $(jira) ]] &&  # If the branch has a ticket ID, attempt to commit
+    git commit -m "$(jira): $1" 
+}
