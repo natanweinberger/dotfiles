9eed5e611f3885ef052c2a419215cddb7f011940
Natan Weinberger <naweinberger+github@gmail.com>
2021-07-09 09:06:42 -0400

    Add mlbv-docker to PATH

--- path.sh
+++ path.sh
@@ -4,5 +4,6 @@ export YTMP3_OUTPUT_DIR=~/Documents/Spotify
 # Add repos to the path
 PATH=$PATH:~/projects/youtube-to-mp3
 PATH=$PATH:~/projects/split-to-tracks
+PATH=$PATH:~/projects/mlbv-docker
 
 export PATH
